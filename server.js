f9155bd66ba1ffa8384dfa97dfb1c38d3732b762
LeoTheMoldyLemon <62692318+LeoTheMoldyLemon@users.noreply.github.com>
2022-04-01 12:59:32 +0200

    HITREG NAPOKON
    PATNJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- server.js
+++ server.js
@@ -22,60 +22,157 @@ app.get('/filelist', async (req, res) => {
 });
 
 players=[]
+tanksize=[22,70]
+
+
+function checkPoint(point, rect){
+    
+    let sides = [[0,0,0],[0,0,0]]
+    let s = [0,0]
+    let dist=0
+    let mindist=1000
+    let minside=[0,0]
+    let triangles=[[[0,1],[2,3]],[[0,3],[2,1]]]
+    for (let i = 0; i<triangles.length; i++){
+        sides[0][0]=distance(rect[triangles[i][0][0]][0], rect[triangles[i][0][0]][1], rect[triangles[i][0][1]][0], rect[triangles[i][0][1]][1])
+        sides[0][1]=distance(rect[triangles[i][0][0]][0], rect[triangles[i][0][0]][1], point[0], point[1])
+        sides[0][2]=distance(rect[triangles[i][0][1]][0], rect[triangles[i][0][1]][1], point[0], point[1])
+        s[0]=(sides[0][0]+sides[0][1]+sides[0][2])/2
+        sides[1][0]=distance(rect[triangles[i][1][0]][0], rect[triangles[i][1][0]][1], rect[triangles[i][1][1]][0], rect[triangles[i][1][1]][1])
+        sides[1][1]=distance(rect[triangles[i][1][0]][0], rect[triangles[i][1][0]][1], point[0], point[1])
+        sides[1][2]=distance(rect[triangles[i][1][1]][0], rect[triangles[i][1][1]][1], point[0], point[1])
+        s[1]=(sides[1][0]+sides[1][1]+sides[1][2])/2
+        dist=distance(rect[triangles[i][0][0]][0], rect[triangles[i][0][0]][1], rect[triangles[i][1][1]][0], rect[triangles[i][1][1]][1])
+        if((dist < (2 / sides[0][0] * Math.sqrt(s[0] * (s[0]-sides[0][0]) * (s[0]-sides[0][1]) * (s[0]-sides[0][2])))) || (dist < (2 / sides[1][0] * Math.sqrt(s[1] * (s[1]-sides[1][0]) * (s[1]-sides[1][1]) * (s[1]-sides[1][2]))))){
+            return(false)
+        }
+        
+    }
+    
+    return(true)
+        
+
+}
+
+function distance(x1,y1,x2,y2){
+    return(Math.sqrt(((x2-x1)**2)+((y2-y1)**2)))
+}
+
+
+function getPoints(x,y,w,h,rot){
+    let r = Math.sqrt((w/2)**2+(h/2)**2)
+    let alpha = Math.acos(h/(2*r))
+    let x1 = r*Math.cos(rot+alpha)
+    let y1 = r*Math.sin(rot+alpha)
+    let x2 = r*Math.cos(rot-alpha)
+    let y2 = r*Math.sin(rot-alpha)
+    return([[x-x1, y+y1],[x+x2, y-y2],[x+x1, y-y1],[x-x2, y+y2]])
+}
 
 
 
-counter=0
-time=new Date()
-thyme=time.getTime()
 async function mainLoop(){
     
-let positions=[[0,0,0,0]]
-    for(i=0; i<players.length; i++){
-        players[i].coords[2]-=0.02*players[i].left
-        players[i].coords[2]+=0.02*players[i].right
-        players[i].coords[0]+=5*players[i].forward*Math.sin(players[i].coords[2])
-        players[i].coords[1]-=5*players[i].forward*Math.cos(players[i].coords[2])
-        players[i].coords[0]-=5*players[i].backward*Math.sin(players[i].coords[2])
-        players[i].coords[1]+=5*players[i].backward*Math.cos(players[i].coords[2])
-        console.log(players[i].mouserot-players[i].coords[3])
-        if(Math.abs(players[i].mouserot-players[i].coords[3])<0.1){
-            players[i].coords[3]=players[i].mouserot
-        }else{
-            if(Math.abs(players[i].mouserot-players[i].coords[3])>Math.PI){
-                players[i].coords[3]-=0.05*Math.sign(players[i].mouserot-players[i].coords[3])
+    let positions=[[0,0,0,0]]
+    let debugpos = getPoints(positions[0][0],positions[0][1],tanksize[0],tanksize[1],positions[0][2])
+    try{
+        for(let i=0; i<players.length; i++){
+            players[i].coords[2]+=0.01*players[i].left
+            players[i].coords[2]-=0.01*players[i].right
+            players[i].coords[0]+=2.5*players[i].forward*Math.cos(players[i].coords[2])
+            players[i].coords[1]-=2.5*players[i].forward*Math.sin(players[i].coords[2])
+            players[i].coords[0]-=2.5*players[i].backward*Math.cos(players[i].coords[2])
+            players[i].coords[1]+=2.5*players[i].backward*Math.sin(players[i].coords[2])
+            
+            let points=getPoints(players[i].coords[0], players[i].coords[1], tanksize[0], tanksize[1], players[i].coords[2])
+            debugpos=debugpos.concat(points)
+            
+            console.log(debugpos)
+            let collision = false
+            for(let j=0; j<4; j++){
+                for(let k=0; k<positions.length; k++){
+                    if(checkPoint(points[j], getPoints(positions[k][0],positions[k][1],tanksize[0],tanksize[1],positions[k][2]))){
+                        collision=true
+                        break
+                    }
+                }
+                if(collision){
+                    players[i].coords[0]-=2.5*players[i].forward*Math.cos(players[i].coords[2])
+                    players[i].coords[1]+=2.5*players[i].forward*Math.sin(players[i].coords[2])
+                    players[i].coords[0]+=2.5*players[i].backward*Math.cos(players[i].coords[2])
+                    players[i].coords[1]-=2.5*players[i].backward*Math.sin(players[i].coords[2])
+                    players[i].coords[2]-=0.01*players[i].left
+                    players[i].coords[2]+=0.01*players[i].right
+                    break
+                }
+            }
+            
+            
+            
+            
+            if(Math.abs(players[i].mouserot-players[i].coords[3])<0.1){
+                players[i].coords[3]=players[i].mouserot
             }else{
-                players[i].coords[3]+=0.05*Math.sign(players[i].mouserot-players[i].coords[3])
+                if(Math.abs(players[i].mouserot-players[i].coords[3])>Math.PI){
+                    players[i].coords[3]-=0.03*Math.sign(players[i].mouserot-players[i].coords[3])
+                }else{
+                    players[i].coords[3]+=0.03*Math.sign(players[i].mouserot-players[i].coords[3])
+                }
+                
+            }
+            
+            if(players[i].coords[3]<0){
+                players[i].coords[3]+=2*Math.PI
+            }else if (players[i].coords[3]>(2*Math.PI)){
+                players[i].coords[3]-=2*Math.PI
             }
             
         }
-        
-        if(players[i].coords[3]<0){
-            players[i].coords[3]+=2*Math.PI
-        }else if (players[i].coords[3]>(2*Math.PI)){
-            players[i].coords[3]-=2*Math.PI
+        for(i=0; i<players.length; i++){
+            await players[i].emit("positionUpdate", players[i].coords)
+            await positions.push(players[i].coords)
         }
-        
+        await io.emit("frameUpdate", positions)
+        await io.emit("debugUpdate", debugpos)
+    }catch{
+        console.log("Skipped frame")
     }
-    for(i=0; i<players.length; i++){
-        await players[i].emit("positionUpdate", players[i].coords)
-        await positions.push(players[i].coords)
-    }
-    await io.emit("frameUpdate", positions)
     
-    
-    counter+=1
-    if(counter%100==0){
-        time=new Date()
-        console.log(counter+" "+(time.getTime()-thyme))
-    }
+
 }
-setInterval(mainLoop, 12)
+setInterval(mainLoop, 10)
 
 
 
 io.sockets.on("connection", (socket)=>{
     console.log("connected")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     socket.coords=[0,0,0,0] //x position, y position, turret rotation, chasis rotation
     socket.forward=false
     socket.backward=false
